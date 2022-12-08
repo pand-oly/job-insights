8f0380627660048fb55ac6c7e06214a27180e2d2
pand-oly <hoa98hoa@gmail.com>
2022-12-08 11:46:10 -0300

    feat(Requirement 10): test_counter function to test count_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+    assert count_ocurrences("data/jobs.csv", "PYTHON") == 1639
+    assert count_ocurrences("data/jobs.csv", "JAVASCRIPT") == 122
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
