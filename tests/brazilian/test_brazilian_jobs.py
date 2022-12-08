e580f6ae8d2b1b8a45190fd5fbaa6a275629b0b4
pand-oly <hoa98hoa@gmail.com>
2022-12-08 14:12:51 -0300

    feat(Requirement 11): test_brazilian_jobs function for test read_brazilian_file function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,6 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translated = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    assert translated in read_brazilian_file("tests/mocks/brazilians_jobs.csv")
